63322580fb2c2c4ba26c09035ba4415748c04b09
andres392455-gif <andres392455@gmail.com>
2025-11-19 01:32:11 -0500

    Crear ControladorProductos.java

---
+++ ControladorProductos.java
@@ -0,0 +1,4 @@
+package avoluxe.controller;
+import avoluxe.model.Carrito;
+public class ControladorCarrito {private static Carrito carrito=new Carrito();
+public static Carrito getCarrito(){return carrito;}}
