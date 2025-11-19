4abafb754c8921771b552c310325f6681710bef8
andres392455-gif <andres392455@gmail.com>
2025-11-19 01:37:26 -0500

    Crear Usuario.java

---
+++ Usuario.java
@@ -0,0 +1,4 @@
+package avoluxe.model;
+public class Usuario {private String usuario,password;
+public Usuario(String u,String p){usuario=u;password=p;}
+public String getUsuario(){return usuario;}public String getPassword(){return password;}}
